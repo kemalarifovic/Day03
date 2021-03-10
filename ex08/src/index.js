53018c87c9db87aa2b26af127f58805255ef16f3
Kemal Arifovic <gigalab@gmail.com>
2021-03-10 15:42:16 +0100

    The Sum of Two Numbers

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -5,4 +5,6 @@ function main ( numOne , numTwo ) {
     return sum ;
     }
     console . log ( main ( 15 , 8 )); // change this line
+    console . log ( main ( 5 , 28 ));
+    console . log ( main ( 150 , 425 ));
     module . exports = main ;
