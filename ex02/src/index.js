c15a45117f20f2787bd69992873fdcb0905865cc
Kemal Arifovic <gigalab@gmail.com>
2021-03-10 11:48:40 +0100

    Commenting

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //Only change code below this line
+    
+    //Jednolinijski komentar
+
+    /*
+    Viselinijski komentar
+    */
+
+    //Only change code above this line
+
+}
+
+main();
+
+module.exports = main;
