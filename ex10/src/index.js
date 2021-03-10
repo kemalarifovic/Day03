d8c2de1c5c9767c9d54032aa32a0b7efd6fb0ce3
Kemal Arifovic <gigalab@gmail.com>
2021-03-10 15:55:05 +0100

    Multiplying Two Numbers

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -5,4 +5,6 @@ function main ( numOne , numTwo ) {
     return product ;
     }
     console . log ( main ( 15 , 3 )); // change this line
+    console . log ( main ( 25 , 8 ));
+    console . log ( main ( 425 , 10 ));
     module . exports = main ;
