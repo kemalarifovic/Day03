af7e90ff76aa886737af55ec5a252f179ec5b26c
Kemal Arifovic <gigalab@gmail.com>
2021-03-12 20:33:52 +0100

    Using the Assignment Operator for Storing Values

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,10 +1,11 @@
-function main(){
-    //Only change code below this line
-    var x,y;
-    x = 10;
-    y = x;
-    //Only change code above this line
-    return[x, y]
+function main() {
+  //Only change code below this line
+  var x;
+  var y;
+  x = 10;
+  y = x;
+  //Only change code above this line
+  return [x, y];
 }
 
 console.log(main());
