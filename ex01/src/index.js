457065a5f0ffa417c2fef74c89279f6156fa6a63
Kemal Arifovic <gigalab@gmail.com>
2021-03-12 20:28:56 +0100

    Hello World!

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,8 +1,7 @@
-function main(){
-    //Only change code below this line
-    console.log('Hello world!');
-    //Only change code above this line
-
+function main() {
+  //Only change code below this line
+  console.log("Hello World!");
+  //Only change code above this line
 }
 
 main();
